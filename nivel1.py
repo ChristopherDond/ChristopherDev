86c47abf3bb8cbdef43537f2cb3049be26d6f49d
ChristopherDev <christopherdondici@gmail.com>
2025-10-24 11:23:06 -0300

    PyChanges1

---
+++ nivel1.py
@@ -0,0 +1,24 @@
+# EX 01
+numero = int(input("Digite um numero aqui:"))
+if numero > 0:
+    print("Numero positivo")
+elif numero < 0:
+    print("Numero negativo")
+else: 
+    print("Numero igual a zero")
+
+# EX 02
+idade = int(input("Digite sua idade aqui:"))
+if idade > 18:
+    print("Maior de 18 anos")
+elif idade < 18:
+    print("Menor de 18 anos")
+else: 
+    print("Tem 18 anos")
+
+# EX 03
+numero = int(input("Digite umm numero aqui:"))
+if numero % 2 == 0:
+    print("Numero par")
+else:
+    print("Numero impar")
